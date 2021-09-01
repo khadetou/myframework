d814117452a58524161fc6ea60a890165fc67279
khadetou <khadetou96@gmail.com>
2021-09-01 11:34:51 +0000

    clean up

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,18 +3,17 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel = $this->model('Post');
     }
     public function index()
     {
-        $post = $this->postModel->getPost();
-        $data = ["title" => "Welcome", "posts" => $post];
+
+        $data = ["title" => "Welcome"];
         $this->view('pages/index', $data);
     }
 
     public function about()
     {
-        $data = ["title" => "About us"];
+        $data = ["title" => "About Us"];
         $this->view('pages/about', $data);
     }
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,3 @@
 <?php require  APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data["title"] ?></h1>
-
-<?php foreach ($data["posts"] as $post) : ?>
-    <li><?php echo $post->title ?></li>
-<?php endforeach; ?>
 <?php require  APPROOT . '/views/inc/footer.php'; ?>
